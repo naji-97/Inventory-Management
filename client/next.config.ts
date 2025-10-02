4e09a64634d0801f8ab00b8f93728a8204c31143
naji-97 <elbairabennaji@gmail.com>
2025-10-02 14:08:04 +0100

    use the static images in public folder instead of S3

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,18 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "s3-inventorymanagment-836071698891.s3.us-east-1.amazonaws.com",
-        port: "",
-        pathname: "/**",
-      },
+  // reactStrictMode: true,
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "s3-inventorymanagment-836071698891.s3.us-east-1.amazonaws.com",
+  //       port: "",
+  //       pathname: "/**",
+  //     },
 
-    ],
-  },
+  //   ],
+  // },
 
 };
 
